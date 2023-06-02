3de181c13ec498c1ffd7a45e52eebaf70cdf451a
Yuri Brito <yuridebatista@gmail.com>
2023-06-01 23:20:47 -0300

    fazendo a tela de login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
+import { useState } from 'react';
 import logoImage from './images/Rustic_Printed-removebg-preview.png'
+import './styles.css'
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div className="App">
-      <div className="wrapLogin">
-        <form className="forLogin">
-          <span className="titleLogin">Boas vindas!</span>
+    <div className='container'>
+      <div className="containerLogin">
+        <div className="wrapLogin">
+          <form className="formLogin">
+            <span className="titleLogin">Boas vindas!</span>
 
-          <span className="titleLogin">
-            <img src={logoImage} ></img>
-          </span>
+            <span className="titleLogin">
+              <img src={logoImage} ></img>
+            </span>
 
-          <div className='inputs'>
-            <input></input>
-            <input></input>
-          </div>
-        </form>
+            <div className='containerInput'>
+              <input className={email !== "" ? 'hasVal inputLogin' : 'inputLogin'} type='email' value={email} onChange={e => setEmail(e.target.value)}/>
+              <span className='focusInput' data-placeholder='Email'></span>
+            </div>
+
+            <div className='containerInput'>
+              <input className={password !== "" ? 'hasVal inputLogin' : 'inputLogin'} type='password' value={password} onChange={e => setPassword(e.target.value)}/>
+              <span className='focusInput' data-placeholder='Password'></span>
+            </div>
+
+            <div className='containerLoginBtn'>
+              <button className='loginFormBtn'>Login</button>
+            </div>
+
+            <div className='textCenter'>
+              <span className='txt1'>Não possui conta?</span>
+              <a className='txt2' href="#">Criar conta.</a>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
