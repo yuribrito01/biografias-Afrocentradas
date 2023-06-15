5bd56051bd9a3a9c04b803542deb7bbaaddaebd5
Yuri Brito <yuridebatista@gmail.com>
2023-06-14 23:50:46 -0300

    criando uma navbar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,17 @@
-
+import { Navbar } from "../../components/navbar"
+import logo from "../../images/Rustic_Printed-removebg-preview.png"
 
 
 
 
 export const Home = () => {
-    return(
-        <h1>Essa vai ser a home</h1>
+    return (
+        <div>
+            <Navbar />
+            <div className="infoHome">
+            <p id="textHome">A cada ano, a Câmara Municipal de Campinas tem a honra de conceder o Diploma Zumbi dos Palmares a um destacado membro da comunidade negra. Essa distinção representa uma homenagem merecida àqueles indivíduos que contribuíram significativamente para o progresso da comunidade negra em Campinas. Este site foi criado com o objetivo de exaltar essas pessoas e seu legado.</p>
+            <img src={logo} alt="Logo" />
+            </div>
+        </div>
     );
 }
