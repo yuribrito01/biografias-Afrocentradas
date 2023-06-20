aba899a42f22718492c3ae0ec6cabdfebc7b266b
Rodrigo <rnogueira@lenovo.com>
2023-06-20 17:13:07 -0300

    incluindo botão de curtir

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -1,6 +1,5 @@
-import "./styles.css"
-import { Link } from "react-router-dom"
-
+import "./styles.css";
+import { Link } from "react-router-dom";
 
 export const Card = ({ image, altImage, name, description }) => {
   return (
@@ -8,7 +7,10 @@ export const Card = ({ image, altImage, name, description }) => {
       <img src={image} alt={altImage} />
       <h2>{name}</h2>
       <p>{description}</p>
-      <button className="moreButton"><Link to={name}>Saiba Mais</Link></button>
+      <button className="likeButton">curtir</button>
+      <button className="moreButton">
+        <Link to={name}>Saiba Mais</Link>
+      </button>
     </div>
-  )
-}
+  );
+};
